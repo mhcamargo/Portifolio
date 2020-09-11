37739ead2e5019ccb6f542791c3917c15af141ac
mhcamargo <murilo.camargo170@gmail.com>
2020-09-11 14:43:50 -0300

    :maple_leaf:

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function () {
-  document.querySelector(".container").classList.toggle("show-menu");
-});
+document
+  .querySelector(".hamburger")
+  .addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+  );
